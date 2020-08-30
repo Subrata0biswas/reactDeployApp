682d6d922898e9d2ebafade02130ce222d5440f3
subrata85 <subratabiswas@digitalaptech.co.in>
2020-08-30 10:58:21 +0530

    to deploy

---
+++ src/user/login/login.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Button, Box, Container } from "@material-ui/core";
+
+function Login() {
+  return (
+    <Container>
+      <Box>
+        <Button variant="contained" color="primary">
+          Button
+        </Button>
+      </Box>
+    </Container>
+  );
+}
+
+export default Login;
